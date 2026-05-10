a43498c7f43715317642264c15d3bae3c00dd2a5
Pratik Bhatt <pratik@Pratik-ka-MacBook-Pro.local>
2026-05-10 23:20:14 +0530

    chore: setup gh-pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
 import NewSimulationPage from './pages/NewSimulationPage';
